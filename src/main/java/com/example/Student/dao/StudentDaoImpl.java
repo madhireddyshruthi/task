634039c8755e9d9441e77f3a82f5a90bd3db3a48
smadhire <smadhire@LIN51016019.corp.capgemini.com>
2023-09-13 07:47:58 +0530

    commiting to workbranch

--- src/main/java/com/example/Student/dao/StudentDaoImpl.java
+++ src/main/java/com/example/Student/dao/StudentDaoImpl.java
@@ -15,21 +15,20 @@ public class StudentDaoImpl implements StudentDao {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 	Logger logger = LoggerFactory.getLogger(StudentDao.class);
-	@Override
-	public StudentDto saveStudentDto(StudentDto studentDto) {
-		String query = "insert into studentVo values('"+studentDto.getStudentId()+"','"+studentDto.getStudentName()+"')";
-		jdbcTemplate.update(query);
-		logger.info("Creating");
-		return studentDto;
-	}
-	@Override
-	public List<StudentDto> getStudentDto() {
-		
-		String sql = "select*from studentVo";
-		logger.info("Find all the data");
-        List<StudentDto> studentDto = jdbcTemplate.query(sql,
-                new BeanPropertyRowMapper<StudentDto>(StudentDto.class));
-        return studentDto;
+	 public StudentDto saveStudentDto(StudentDto studentDto) {
+	        String insertQuery = "INSERT INTO student (studentId, studentName) VALUES (?, ?)";
+	        jdbcTemplate.update(insertQuery, studentDto.getStudentId(), studentDto.getStudentName());
+	        logger.info("Inserting data into the student table");
+	        return studentDto;
+	    }
+
+	    @Override
+	    public List<StudentDto> getStudentDto() {
+	        String sql = "SELECT * FROM student";
+	        logger.info("Find all the data");
+	        List<StudentDto> studentDto = jdbcTemplate.query(sql,
+	                new BeanPropertyRowMapper<>(StudentDto.class));
+	        return studentDto;
 	
        
 }
